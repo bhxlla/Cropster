7608c5586fb3f70c05c678372f5d8d43f29f98bd
Varun <varunbhalla19@gmail.com>
2023-08-05 12:51:35 +0530

    initial commit

---
+++ Cropster/CropsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CropsterApp.swift
+//  Cropster
+//
+//  Created by Varun on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CropsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
