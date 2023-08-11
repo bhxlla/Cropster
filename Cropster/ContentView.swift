53586dec02c6e96d735277888bf63dfab8e8ac61
Varun <varunbhalla19@gmail.com>
2023-08-11 13:23:49 +0530

    setup NavigationView to either show image list or image editor.

--- Cropster/ContentView.swift
+++ Cropster/ContentView.swift
@@ -8,19 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var image: UIImage? = nil
+    @State var showDetail: Bool = false
+    
+    @State var initShow = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            
+            ZStack {
+                if initShow {
+                    IntroView.init { img in
+                        image = img
+                        showDetail = true
+                    }
+                    .transition(.opacity.combined(with: .move(edge: .bottom)))
+                }
+            }
+            .background {
+                NavigationLink(
+                    destination: CropsterScreen(image: $image),
+                    isActive: $showDetail
+                ) {
+                    EmptyView()
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: .init(block: {
+                    withAnimation {
+                        initShow = true
+                    }
+                }))
+            }
+            
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
